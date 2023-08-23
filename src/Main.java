8580df6f12bdeb8ef35d6d2e7bba844d72e9269a
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2023-08-23 12:04:06 +0200

    första push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int a = (int) (Math.random() * 51) +1;
+        int b = (int) (Math.random() * 51) +1;
+        System.out.print("press enter to play");
+        String enter = scan.nextLine();
+        if (a<b){
+            System.out.println("a got "+a+" and b got "+b);
+            System.out.println("b won");
+            return;
+        }
+        if (a>b){
+            System.out.println("a got "+a+" and b got "+b);
+            System.out.println("a won");
+            return;
+        }
+        System.out.println("kör igen");
     }
 }
