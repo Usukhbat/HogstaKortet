631115e1b90ec355a8f4149088f2c2e2fee4e51c
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2023-09-04 09:40:45 +0200

    första push

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,76 @@
 import java.util.Scanner;
 
+// ┌ ┘ ┗ ┓ ─
 public class Main {
+    public static void mainGame(int a, int b, int[] normalCards) {
+        for (int i = 0; i < 9; i++) {
+            if (a == i) {
+                System.out.println("          ┌────┓");
+                System.out.println("Player 1: |  " + a + " |");
+                System.out.println("          ┗────┘");
+            }
+            if (b == i) {
+                System.out.println("          ┌────┓");
+                System.out.println("Player 2: |  " + b + " |");
+                System.out.println("          ┗────┘");
+            }
+        }
+    }
+
+    public static void specialGame(int a, int b, String[] specialCards) {
+        for (int i = 0; i < 4; i++) {
+            if (a - 1 == i) {
+                System.out.println("          ┌────┓");
+                System.out.println("Player 1: |  " + specialCards[a] + " |");
+                System.out.println("          ┗────┘");
+            }
+            if (b - 1 == i) {
+                System.out.println("          ┌────┓");
+                System.out.println("Player 1: |  " + specialCards[b] + " |");
+                System.out.println("          ┗────┘");
+            }
+        }
+    }
+
+    public static int playAgain(String answer) {
+        if (!answer.equals("yes")) {
+            System.out.println("Goodbye!");
+            return 2;
+        }
+        return 1;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = (int) (Math.random() * 51) +1;
-        int b = (int) (Math.random() * 51) +1;
-        System.out.print("press enter to play");
-        String enter = scan.nextLine();
-        if (a<b){
-            System.out.println("a got "+a+" and b got "+b);
-            System.out.println("b won");
-            return;
+        System.out.println("Card Game");
+        System.out.print("Press enter to play...");
+        scan.nextLine();
+        int game = 1;
+        int[] normalCards = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        String[] specialCards = {"J", "Q", "K", "A"};
+        while (game == 1) {
+            int a = (int) (Math.random() * 13) + 1;
+            int b = (int) (Math.random() * 13) + 1;
+            if (a < b) {
+                mainGame(a, b, normalCards);
+                specialGame(a, b, specialCards);
+                System.out.println("Player 2 won!");
+                System.out.println("Play again? (Type yes to continue.)");
+                String answer = scan.next();
+                game = playAgain(answer);
+            }
+            if (a > b) {
+                mainGame(a, b, normalCards);
+                specialGame(a, b, specialCards);
+                System.out.println("Player 1 won!");
+                System.out.println("Play again? (Type yes to continue.)");
+                String answer = scan.next();
+                game = playAgain(answer);
+            }
+            if (a == b) {
+                System.out.println("Draw, play again.");
+                scan.nextLine();
+            }
         }
-        if (a>b){
-            System.out.println("a got "+a+" and b got "+b);
-            System.out.println("a won");
-            return;
-        }
-        System.out.println("kör igen");
     }
 }
