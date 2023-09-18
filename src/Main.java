8b57658fd63f862ed8072fb6d8d27c3ac3ce22dc
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2023-09-18 08:52:33 +0200

    fixade till så att mitt kort system fungerade.har mekat med det här den senaste 3 lektionerna.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // ┌ ┘ ┗ ┓ ─
 public class Main {
-    public static void mainGame(int a, int b, int[] normalCards) {
+    public static void mainGame(int a, int b) {
         for (int i = 0; i < 9; i++) {
             if (a == i) {
                 System.out.println("          ┌────┓");
@@ -19,14 +19,16 @@ public class Main {
 
     public static void specialGame(int a, int b, String[] specialCards) {
         for (int i = 0; i < 4; i++) {
-            if (a - 1 == i) {
+            if (a > 9) {
+                a = a - 9;
                 System.out.println("          ┌────┓");
                 System.out.println("Player 1: |  " + specialCards[a] + " |");
                 System.out.println("          ┗────┘");
             }
-            if (b - 1 == i) {
+            if (b > 9) {
+                b = b - 9;
                 System.out.println("          ┌────┓");
-                System.out.println("Player 1: |  " + specialCards[b] + " |");
+                System.out.println("Player 2: |  " + specialCards[b] + " |");
                 System.out.println("          ┗────┘");
             }
         }
@@ -46,13 +48,12 @@ public class Main {
         System.out.print("Press enter to play...");
         scan.nextLine();
         int game = 1;
-        int[] normalCards = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         String[] specialCards = {"J", "Q", "K", "A"};
         while (game == 1) {
             int a = (int) (Math.random() * 13) + 1;
             int b = (int) (Math.random() * 13) + 1;
             if (a < b) {
-                mainGame(a, b, normalCards);
+                mainGame(a, b);
                 specialGame(a, b, specialCards);
                 System.out.println("Player 2 won!");
                 System.out.println("Play again? (Type yes to continue.)");
@@ -60,7 +61,7 @@ public class Main {
                 game = playAgain(answer);
             }
             if (a > b) {
-                mainGame(a, b, normalCards);
+                mainGame(a, b);
                 specialGame(a, b, specialCards);
                 System.out.println("Player 1 won!");
                 System.out.println("Play again? (Type yes to continue.)");
